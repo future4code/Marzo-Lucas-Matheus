0f4750de93ab0f82c37b3cbb977715f84146486b
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-01 12:40:21 -0300

    Exercício 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -84,7 +84,13 @@ function retornaSegundoMaiorESegundoMenor(array) {
 }
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {}
+function retornaChamadaDeFilme(filme) {
+  return `Venha assistir ao filme ${filme.nome}, de ${
+    filme.ano
+  }, dirigido por ${filme.diretor} e estrelado por${filme.atores.map(
+    (ator) => ` ${ator}`
+  )}.`;
+}
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {}
