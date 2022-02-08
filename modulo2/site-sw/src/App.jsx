33b89b873ba83408b9a8c5a3d4c9ce592eb9213d
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-08 13:11:03 -0300

    Setup do Projeto

---
+++ modulo2/site-sw/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Star Wars</h1>
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default App;
