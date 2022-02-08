49c40eb0ea341920e644810270e21fa707bad0c7
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-08 14:00:21 -0300

    Lista de Personagens

--- modulo2/site-sw/src/pages/CharacterListPage/index.jsx
+++ modulo2/site-sw/src/pages/CharacterListPage/index.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
+import { Container } from "./styles";
 
 export const CharacterListPage = () => {
   const navigate = useNavigate();
 
+  const [characterList, setCharacterList] = useState([]);
+
+  function getCharacterList() {
+    api.get("people").then((response) => {
+      console.log(response.data.results);
+      setCharacterList(response.data.results);
+    });
+  }
+
+  useEffect(() => {
+    getCharacterList();
+  }, []);
+
   return (
-    <div>
-      CharacterListPage
+    <Container>
+      {characterList.map((char) => (
+        <p key={char.name}>{char.name}</p>
+      ))}
+
       <button onClick={() => navigate(`/details/a`)}>List</button>
-    </div>
+    </Container>
   );
 };
