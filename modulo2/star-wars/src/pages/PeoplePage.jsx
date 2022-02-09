4e6b1801f7fd240ac382184725c3dda3ac810504
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-09 15:08:07 -0300

    PeoplePage

--- modulo2/star-wars/src/pages/PeoplePage.jsx
+++ modulo2/star-wars/src/pages/PeoplePage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Card } from "../components/Card";
+import { api } from "../services/api";
+import person from "../assets/person.png";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Grid = styled.div`
+  padding: 4rem 3rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+`;
 
 export const PeoplePage = () => {
-  return <div>PeoplePage</div>;
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    api.get("people").then(({ data }) => setPeople(data.results));
+  }, []);
+
+  return (
+    <Container>
+      <Grid>
+        {people.map(({ name }) => (
+          <Card name={name} image={person} />
+        ))}
+      </Grid>
+    </Container>
+  );
 };
