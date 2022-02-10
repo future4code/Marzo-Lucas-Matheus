f07c2b7a17bd224cfb191a8ce0bae555629a3530
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-10 16:58:03 -0300

    Estilo Global

---
+++ modulo2/tarot/src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  text-area,
+  button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
