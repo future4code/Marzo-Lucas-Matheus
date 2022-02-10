2c52090d99f012b6df2cc45af0f565b984352726
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-10 17:15:31 -0300

    Componente Header

--- modulo2/tarot/src/components/Header.jsx
+++ modulo2/tarot/src/components/Header.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.header`
+  border-bottom: 1px solid black;
+  text-align: center;
+  padding: 0.75rem;
+  background: transparent;
+  position: sticky;
+  top: 0;
+
+  button {
+    padding: 0.5rem 1.5rem;
+
+    background: #1a8cd8;
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    font-weight: 600;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <button>Começar</button>
+    </Container>
+  );
 };
