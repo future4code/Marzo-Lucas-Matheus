f8e937d6f30f47d0ca27ffdfc4e14fd9425c8eaa
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-17 17:14:36 -0300

    Funcionamento do filtro de pesquisa

--- modulo3/goomer/src/pages/Homepage/index.jsx
+++ modulo3/goomer/src/pages/Homepage/index.jsx
@@ -14,22 +14,29 @@ import { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { HomepageCard } from "../../components";
 
-export const Homepage = () => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios.get("http://challange.goomer.com.br/restaurants").then(({ data }) => {
-      setRestaurants(data);
-      setLoading(false);
-    });
-  }, []);
+export const Homepage = ({ restaurants, loading }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  function filterRestaurants(restaurant) {
+    if (searchTerm === "") return true;
+    if (
+      restaurant.name.includes(searchTerm) ||
+      restaurant.name.toLowerCase().includes(searchTerm)
+    )
+      return true;
+    return false;
+  }
   return (
     <Container centerContent maxW="container.lg">
       <Heading my="8" color="#404040" size="lg">
         Bem-vindo ao Lista Rango
       </Heading>
       <InputGroup my="8" size="md" w="80%" boxShadow="0px 2px 4px #00000029">
-        <Input p="4" placeholder="Buscar estabelecimento" />
+        <Input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          p="4"
+          placeholder="Buscar estabelecimento"
+        />
         <InputRightElement children={<BsSearch />} />
       </InputGroup>
 
@@ -43,7 +50,7 @@ export const Homepage = () => {
         />
       ) : (
         <SimpleGrid my="8" columns={[1, 2, 3]} spacing={[4, 8]}>
-          {restaurants.map((restaurant) => (
+          {restaurants.filter(filterRestaurants).map((restaurant) => (
             <HomepageCard key={restaurant.id} restaurant={restaurant} />
           ))}
         </SimpleGrid>
--- modulo3/goomer/src/router.jsx
+++ modulo3/goomer/src/router.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage, Detailspage } from "./pages";
 
 export const Router = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get("http://challange.goomer.com.br/restaurants").then(({ data }) => {
+      setRestaurants(data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/"
+          element={<Homepage restaurants={restaurants} loading={loading} />}
+        />
         <Route path="/details/:id" element={<Detailspage />} />
       </Routes>
     </BrowserRouter>
