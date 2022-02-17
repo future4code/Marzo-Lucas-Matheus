fa6eb59be987290b9253ad10910471f107545b1e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-17 11:42:59 -0300

    Homepage SEM grid com cards

--- modulo3/goomer/src/pages/Homepage/index.jsx
+++ modulo3/goomer/src/pages/Homepage/index.jsx
@@ -1,5 +1,23 @@
+import {
+  Container,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
 import React from "react";
 
 export const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Container centerContent maxW="container.lg">
+      <Heading my="8" color="#404040" size="lg">
+        Bem-vindo ao Lista Rango
+      </Heading>
+      <InputGroup size="md" w="80%" boxShadow="0px 2px 4px #00000029">
+        <Input p="4" placeholder="Buscar estabelecimento" />
+        <InputRightElement children={<BsSearch />} />
+      </InputGroup>
+    </Container>
+  );
 };
