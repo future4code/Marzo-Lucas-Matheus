2902f4ad0338d2572225796d095e3e6200d3fff9
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-18 23:27:09 -0300

    Página Result

--- modulo3/ciclic/src/components/Result.jsx
+++ modulo3/ciclic/src/components/Result.jsx
@@ -1,5 +1,19 @@
+import { Button, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
-export const Result = () => {
-  return <div>Result</div>;
+export const Result = ({ setCurrentPage, name, payment, time, result }) => {
+  return (
+    <Stack my={8}>
+      <Text fontSize="1.5rem">{`Olá ${name}, juntando R$${payment} todo mês, você terá R$${result} em ${time} meses.`}</Text>
+      <Button
+        fontFamily="mono"
+        textTransform="uppercase"
+        size="lg"
+        colorScheme="blue"
+        onClick={() => setCurrentPage("home")}
+      >
+        Simular Novamente
+      </Button>
+    </Stack>
+  );
 };
