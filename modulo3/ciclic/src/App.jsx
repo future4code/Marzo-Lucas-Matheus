949d0ebb7265d5befca7c6490d67295d6885659c
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-18 22:18:48 -0300

    Renderização das telas de acordo com o estado

--- modulo3/ciclic/src/App.jsx
+++ modulo3/ciclic/src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Home, Result } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  const [currentPage, setCurrentPage] = useState("home");
+
+  function renderPage() {
+    switch (currentPage) {
+      case "home":
+        return <Home />;
+      case "result":
+        return <Result />;
+    }
+  }
+
+  return <>{renderPage()}</>;
 };
 
 export default App;
