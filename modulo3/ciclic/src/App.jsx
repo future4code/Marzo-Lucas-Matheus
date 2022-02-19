bae596f70e56c69fc9e94265be1ee7ddf447746f
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-02-18 23:15:27 -0300

    Subindo os estados e a requisição para poder servir aos componentes filhos

--- modulo3/ciclic/src/App.jsx
+++ modulo3/ciclic/src/App.jsx
@@ -1,16 +1,48 @@
 import { Container } from "@chakra-ui/react";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Header, Home, Result } from "./components";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("home");
+  const [name, setName] = useState("");
+  const [payment, setPayment] = useState("");
+  const [time, setTime] = useState(0);
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    const body = {
+      expr: `${payment} * (((1 + 0.00517) ^ ${time} - 1) / 0.00517)`,
+    };
+    axios.post("http://api.mathjs.org/v4/", body).then(({ data }) => {
+      setResult(Number(data.result).toFixed(2));
+    });
+  }, [payment, time]);
 
   function renderPage() {
     switch (currentPage) {
       case "home":
-        return <Home />;
+        return (
+          <Home
+            setCurrentPage={setCurrentPage}
+            name={name}
+            setName={setName}
+            payment={payment}
+            setPayment={setPayment}
+            time={time}
+            setTime={setTime}
+          />
+        );
       case "result":
-        return <Result />;
+        return (
+          <Result
+            setCurrentPage={setCurrentPage}
+            name={name}
+            payment={payment}
+            time={time}
+            result={result}
+          />
+        );
     }
   }
 
